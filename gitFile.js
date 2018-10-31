075b259d61182a193bade2c0accc88cba957c2e3
Satch09 <18578121+Satch09@users.noreply.github.com>
2018-10-31 20:14:42 +0200

    added new feature

--- gitFile.js
+++ gitFile.js
@@ -2,4 +2,5 @@
 // This is a test
 // So I've just added another line to test the git pull function
 console.log('this a console log to test merge conflict AB');
+console.log('this is a new feature that I would like to add to the application.');
 // Foo
