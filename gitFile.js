cda1fa8e09534ed4ba4beb572e11723db5e6d1f4
Satch09 <18578121+Satch09@users.noreply.github.com>
2018-10-31 20:37:16 +0200

    cleaned up my tests

--- gitFile.js
+++
@@ -1,6 +0,0 @@
-// This is just a test file that I am adding in order to test the git CLI functionality
-// This is a test
-// So I've just added another line to test the git pull function
-console.log('this a console log to test merge conflict AB');
-console.log('this is a new feature that I would like to add to the application.');
-// Foo
--- newFeature.js
+++
@@ -1,3 +0,0 @@
-//This is a new feature file that adds some extra functionality to the application.
-//This is some other changes that need to be added to the version that I would like.
-//After some testing, I think that this is ready to merge with the master branch.
