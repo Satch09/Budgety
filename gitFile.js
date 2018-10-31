95ea06935d2feaa2b55485937c62a640d7680811
Satch09 <christophermaisch@gmail.com>
2018-10-31 19:01:53 +0200

    Another push

--- gitFile.js
+++ gitFile.js
@@ -1,3 +1,4 @@
 //This is just a test file that I am adding in order to test the git CLI functionality
 //This is a test
 //So I've just added another line to test the git pull function
+console.log('this a console log to test!');
