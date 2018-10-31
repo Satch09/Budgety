655c7687d233997b0ed8bf3f251471af6a4e1d41
Satch09 <christophermaisch@gmail.com>
2018-10-31 19:09:50 +0200

    merge conflict testingo

--- gitFile.js
+++ gitFile.js
@@ -1,5 +1,5 @@
-//This is just a test file that I am adding in order to test the git CLI functionality
-//This is a test
-//So I've just added another line to test the git pull function
-console.log('this a console log to test!');
-//Foo
+// This is just a test file that I am adding in order to test the git CLI functionality
+// This is a test
+// So I've just added another line to test the git pull function
+console.log('this a console log Merge Conflict B');
+// Foo
